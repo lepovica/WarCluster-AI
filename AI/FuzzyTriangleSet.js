7c3089c29d5004d2671621e96f3c8198dd32c26e
lepovica <lepo333@abv.bg>
2015-04-25 17:06:38 +0300

    add functionality to the sets and add Fuzzy Variable

--- AI/FuzzyTriangleSet.js
+++ AI/FuzzyTriangleSet.js
@@ -1,24 +1,35 @@
-function FuzzyTriangleSet(leftOffset, peakPoint, rightOffset) {
-	return {
+module.exports = function(leftOffset, peakPoint, rightOffset) {
+	var core = {
+		_DOM : 0.0,	
 		_peakPoint : peakPoint,
 		_leftOffset : leftOffset,
 		_rightOffset : rightOffset,
 		calculateDOM : function(value) {
-					if( ((rightOffset === 0.0) && (peakPoint === value)) || ((leftOffset === 0.0) && (peakPoint === value)) ) {
-						return 1.0;
-					}
-					if( (value <= peakPoint) && (value >= (peakPoint - leftOffset)) ){
-						var grad = 1.0 / leftOffset;
-						return grad * (value - (peakPoint - leftOffset));
-							
-					}else {
-						if( (value > peakPoint) && (value < (peakPoint + rightOffset))) {
-							var grad = 1.0 / -rightOffset;
-							return grad * (value - peakPoint) + 1.0;
-						}else {
-							return 0.0;
-						}
-					}
+			if( ((rightOffset === 0.0) && (peakPoint === value)) || ((leftOffset === 0.0) && (peakPoint === value)) ) {
+				return 1.0;
+			}
+			if( (value <= peakPoint) && (value >= (peakPoint - leftOffset)) ){
+				var grad = 1.0 / leftOffset;
+				return grad * (value - (peakPoint - leftOffset));
+					
+			}else {
+				if( (value > peakPoint) && (value < (peakPoint + rightOffset))) {
+					var grad = 1.0 / -rightOffset;
+					return grad * (value - peakPoint) + 1.0;
+				}else {
+					return 0.0;
 				}
+			}
+		},
+		clearDOM : function() {
+			core._DOM = 0.0;
+		},
+		setDOM : function(value) {
+			core._DOM = value;
+		},
+		getDOM : function(value) {
+			return core._DOM;
 		}
+	}
+	return core;
 }
