ddaa5e93432ed71b619afc5d64b2a82a79d24f66
Vitaliy Filipov <altras@gmail.com>
2015-04-25 14:58:05 +0300

    use comparison not equal

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ var tokens = require("./tokens.js");
 
 process.env.CELL_MODE = process.env.NODE_ENV || process.env.CELL_MODE || "development";
 
-if (process.env.CELL_MODE = "development") {
+if (process.env.CELL_MODE == "development") {
   config = require("./config/development.js");
-} else if (process.env.CELL_MODE = "staging") {
+} else if (process.env.CELL_MODE == "staging") {
   config = require("./config/staging.js");
-} else if (process.env.CELL_MODE = "hackfmi") {
+} else if (process.env.CELL_MODE == "hackfmi") {
   config = require("./config/hackfmi.js")
 }
 console.log(config.socketUrl)
